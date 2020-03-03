4d83bbb9e644af13eb834841e203ef1e198e79f4
Trivedi Bodlapati <trivedikumar.b@gmail.com>
2020-03-02 23:02:54 -0600

    added dutchFlagSeparation algorithm

--- src/test/java/com/sample/test/algorithms/ArrayAlgorithmsTest.java
+++ src/test/java/com/sample/test/algorithms/ArrayAlgorithmsTest.java
@@ -15,4 +15,16 @@ public class ArrayAlgorithmsTest {
         Assert.assertEquals(5, array[3]);
         Assert.assertEquals(1, array[4]);
     }
+
+    @Test
+    public void testDutchFlagSeparation() {
+        int[] array = new int[] { 1, 2, 4, 5, 2, 0};
+        ArrayAlgorithms.dutchFlagSeparation(array, 1);
+        Assert.assertEquals(0, array[0]);
+        Assert.assertEquals(1, array[1]);
+        Assert.assertEquals(2, array[2]);
+        Assert.assertEquals(2, array[3]);
+        Assert.assertEquals(4, array[4]);
+        Assert.assertEquals(5, array[5]);
+    }
 }
